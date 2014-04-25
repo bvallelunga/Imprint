ba1ca5e07e3a5ef4ecb4ede439096e733c3fec21
Brian Vallelunga <vallelungabrian@gmail.com>
2014-04-25 01:02:00 -0700

    added project selection

--- public/js/dashboard/index.js
+++ public/js/dashboard/index.js
@@ -1,4 +1,24 @@
 $(function() {
+    $(".header .project").click(function() {
+        var _this = this;
+
+        if($(_this).hasClass("activated")) {
+            $(".projects").removeClass("activated");
+
+            setTimeout(function() {
+                $(_this).removeClass("activated");
+                $(".projects").slideUp(300);
+            }, 300);
+        } else {
+            $(_this).addClass("activated");
+            $(".projects").slideDown(300);
+
+            setTimeout(function() {
+                $(".projects").addClass("activated");
+            }, 500);
+        }
+    });
+
     $(".header .profile").click(function(e) {
         e.stopPropagation();
         $(this).toggleClass("activated");
